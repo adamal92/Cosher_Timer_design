95bfe72e0d6e581563964517523adc25b8ae59df
Adamal92 <Livneadam@gmail.com>
2020-10-31 02:10:27 +0200

    first bug fix

--- app/src/main/java/adam/cosher_timer/Timer.java
+++ app/src/main/java/adam/cosher_timer/Timer.java
@@ -1,20 +1,16 @@
 package adam.cosher_timer;
 
 import android.annotation.SuppressLint;
-import android.appwidget.AppWidgetManager;
 import android.os.Build;
 import android.os.CountDownTimer;
-import android.text.Html;
 import android.widget.RemoteViews;
 import android.widget.TextView;
 
 import androidx.annotation.RequiresApi;
 
-import static adam.cosher_timer.MainActivity.alert;
-
 /**
 * This class is responsible for presenting the time to the user
-* @version 1.0
+* @version 2.0
 * @author Adam L
 */
 public class Timer extends CountDownTimer {
@@ -56,6 +52,7 @@ public class Timer extends CountDownTimer {
     }
 
     /**
+     * Updates the text of the clock every second
      * Gets called on every tick of the timer
      * @param millisUntilFinished the remaining amount of time in milliseconds
      */
@@ -63,9 +60,9 @@ public class Timer extends CountDownTimer {
     @SuppressLint({"DefaultLocale", "SetTextI18n"})
     @Override
     public void onTick(long millisUntilFinished) {
-        // TODO: print the time to the widget TextView
         Timer.stopped_on_millis = millisUntilFinished;
 
+        // calculate the current time
         long secsUntilFinished = millisUntilFinished / 1000;
         long hours = secsUntilFinished / 3600;
         long minutes = secsUntilFinished / 60 - hours * 60;
@@ -74,42 +71,37 @@ public class Timer extends CountDownTimer {
         long hundredth_seconds = milliseconds / 10;
         long milli = milliseconds % 10;
 
+        // generate Strings of the current time
         String h = hours >= 10 ? String.valueOf(hours) : "0" + hours;
         String m = minutes >= 10 ? String.valueOf(minutes) : "0" + minutes;
         String s = seconds >= 10 ? String.valueOf(seconds) : "0" + seconds;
         String hs = hundredth_seconds >= 10 ? String.valueOf(hundredth_seconds) : "0" + hundredth_seconds;
-        String ms = milli >= 10 ? String.valueOf(milli) : "0" + milli;
+        String ms = "0" + milli; // milli < 10 will always be true
 
-        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            this.timerOutput.setText(android.text.Html
-                    .fromHtml(String.format(MainActivity.COUNTING+"<br/>%s:%s:%s", h, m, s), Html.FROM_HTML_MODE_LEGACY));
-            this.widget_views.setTextViewText(R.id.timer_out_widget, android.text.Html
-                    .fromHtml(String.format(MainActivity.COUNTING+"<br/>%s:%s:%s", h, m, s), Html.FROM_HTML_MODE_LEGACY));
-        } else {
-            this.timerOutput.setText(MainActivity.COUNTING+String.format("\n%s:%s:%s", h, m, s));
-            this.widget_views.setTextViewText(R.id.timer_out_widget, MainActivity.COUNTING+String.format("\n%s:%s:%s", h, m, s));
-        }
-//        this.timerOutput.setText(String.format("counting time\n%s:%s:%s", h, m, s));
-//        String.format("counting time  %s:%s:%s:%s:%s", h, m, s,hs, ms)
-
-        //
-        RemoteViews views = new RemoteViews(StartTimerActivity.start_context.getPackageName(), R.layout.widget_layout);
+        // set text of the main textView
+        this.timerOutput.setText(MainActivity.COUNTING+String.format("\n%s:%s:%s", h, m, s));
+        // set text of the widgets' textView
+        RemoteViews views = new RemoteViews(MainActivity.PACKAGE_NAME, R.layout.widget_layout); // StartTimerActivity.start_context.getPackageName()
         views.setTextViewText(R.id.timer_out_widget, MainActivity.COUNTING+String.format("\n%s:%s:%s", h, m, s));
-
-//        WidgetTimer.widgetTimer.onUpdate(WidgetTimer.onUpdate_context,
-//                WidgetTimer.onUpdate_appWidgetManager, WidgetTimer.onUpdate_appWidgetIds);
-        StartTimerActivity.start_appWidgetManager.updateAppWidget(WidgetTimer.WidgetId, views);
-
-
+        // update the widget
+        if(WidgetTimer.widget_appWidgetManager != null)
+            WidgetTimer.widget_appWidgetManager.updateAppWidget(WidgetTimer.WidgetId, views);
     }
 
     /**
+     * Set the clocks' text to 'DONE', update the widget & alert the user
      * Gets called when the Timer is cancelled or when millisUntilFinished is 0
      */
     @Override
     public void onFinish() {
         this.timerOutput.setText(MainActivity.DONE);
-        this.widget_views.setTextViewText(R.id.timer_out_widget, MainActivity.DONE);
+//        this.widget_views.setTextViewText(R.id.timer_out_widget, MainActivity.DONE);
+
+        RemoteViews views = new RemoteViews(MainActivity.PACKAGE_NAME, R.layout.widget_layout);
+        views.setTextViewText(R.id.timer_out_widget, MainActivity.DONE);
+        if(WidgetTimer.widget_appWidgetManager != null)
+            WidgetTimer.widget_appWidgetManager.updateAppWidget(WidgetTimer.WidgetId, views);
+
         MainActivity.alert(MainActivity.mainContext);
     }
 
--- app/src/main/java/adam/cosher_timer/WidgetTimer.java
+++ app/src/main/java/adam/cosher_timer/WidgetTimer.java
@@ -9,18 +9,21 @@ import android.content.Intent;
 import android.util.Log;
 import android.widget.RemoteViews;
 
+/**
+ * Create the widgets of the app
+ * @version 2.0
+ * @author Adam L
+ */
 public class WidgetTimer extends AppWidgetProvider {
-    private Timer timer;
-    private boolean stopped_timer;
     private static final String ACTION_NAME = "start_timer";
     public static int WidgetId = 0;
     public static WidgetTimer widgetTimer = null;
-    public static Context onUpdate_context = null;
-    public static AppWidgetManager onUpdate_appWidgetManager = null;
-    public static int[] onUpdate_appWidgetIds = null;
+    public static Context widget_context = null;
+    public static AppWidgetManager widget_appWidgetManager = null;
 
     /**
-     * enables the widget buttons
+     * Enable the widget buttons' action (the 'click')
+     * and update the widget
      * @param context
      * @param appWidgetManager
      * @param appWidgetId
@@ -29,7 +32,7 @@ public class WidgetTimer extends AppWidgetProvider {
                                 int appWidgetId, int[] appWidgetIds) {
         Log.d("Adam", "WidgetTimer.updateAppWidget()");
         WidgetTimer.WidgetId = appWidgetId;
-        // Construct the RemoteViews object
+        // Construct the RemoteViews object. views - the widget_layout
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
 
         // Create an Intent to launch ExampleActivity when clicked
@@ -37,68 +40,44 @@ public class WidgetTimer extends AppWidgetProvider {
         Intent intent_stop = new Intent(context, StopTimerActivity.class);
         Intent intent_start = new Intent(context, StartTimerActivity.class);
 
+        // set an update action
         intent_stop.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
         intent_start.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
 
+        // register the widgets' id
         intent_stop.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
         intent_start.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
 
-        PendingIntent pendingIntent_stop = PendingIntent.getBroadcast(context,0, intent_stop, PendingIntent.FLAG_UPDATE_CURRENT);
-        PendingIntent pendingIntent_start = PendingIntent.getBroadcast(context,0, intent_start, PendingIntent.FLAG_UPDATE_CURRENT);
+        // get an update broadcast
+        PendingIntent pendingIntent_stop = PendingIntent
+                .getBroadcast(context,0, intent_stop, PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingIntent pendingIntent_start = PendingIntent
+                .getBroadcast(context,0, intent_start, PendingIntent.FLAG_UPDATE_CURRENT);
 
+        // set click listener
         views.setOnClickPendingIntent(R.id.stop_clock_widget, pendingIntent_stop);
         views.setOnClickPendingIntent(R.id.start_clock_widget, pendingIntent_start);
 
-        // Instruct the widget manager to update the widget
+        // instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
     /**
-     * Enables the Start button
+     * Initialise every instance of the widget
      * @param context
      * @param appWidgetManager
-     * @param appWidgetId
      * @param appWidgetIds
      */
-    static void update_StartTimer_button(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId, int[] appWidgetIds) {
-        Log.d("Adam", "WidgetTimer.update_StartTimer_button()");
-        WidgetTimer.WidgetId = appWidgetId;
-        // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
-        // Set the text of TextView
-//        WidgetTimer.a++;
-//        views.setTextViewText(R.id.timer_out_widget, String.valueOf(WidgetTimer.a));
-        // Create an Intent to launch ExampleActivity when clicked
-        // Register an onClickListener
-        Intent intent_one = new Intent(context, StartTimerActivity.class);
-
-        intent_one.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
-        intent_one.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
-
-        PendingIntent pendingIntent_one = PendingIntent.getBroadcast(context,0, intent_one, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        views.setOnClickPendingIntent(R.id.start_clock_widget, pendingIntent_one);
-
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
-    }
-
-        /**
-         *7
-         * @param context
-         * @param appWidgetManager
-         * @param appWidgetIds
-         */
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         super.onUpdate(context, appWidgetManager, appWidgetIds);
         Log.d("Adam", "WidgetTimer.onUpdate()");
-//        MainActivity.alert(MainActivity.mainContext);
-        WidgetTimer.widgetTimer = this;
-        WidgetTimer.onUpdate_context = context;
-        WidgetTimer.onUpdate_appWidgetManager = appWidgetManager;
-        WidgetTimer.onUpdate_appWidgetIds = appWidgetIds;
+
+        WidgetTimer.widgetTimer = this; // create an intended memory leak
+
+        // initialize static values
+        WidgetTimer.widget_context = context;
+        WidgetTimer.widget_appWidgetManager = appWidgetManager;
 
         // Get all ids
         ComponentName thisWidget = new ComponentName(context, WidgetTimer.class);
@@ -107,17 +86,6 @@ public class WidgetTimer extends AppWidgetProvider {
         // There may be multiple widgets active, so update all of them
         for (int appWidgetId : all_WidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId, appWidgetIds); // enables the widget buttons
-//            update_StartTimer_button(context, appWidgetManager, appWidgetId, appWidgetIds); // enables the start button
-        }
-    }
-
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        super.onReceive(context, intent);
-        Log.d("Adam", "WidgetTimer.onReceive()");
-
-        if (ACTION_NAME.equals(intent.getAction())) {
-            Log.d("Adam", "MADAFACKA!!!!!!!!!!!!!!!!");
         }
     }
 }
